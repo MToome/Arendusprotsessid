72e96676d89438cf447a6b4f45ee85393244c34c
Marek Toome <marekjah@gmail.com>
2025-05-31 06:20:07 +0300

    add fetch Rest api data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.scss';
 
 import * as bootstrap from 'bootstrap';
+
+fetch('https://api.chucknorris.io/jokes/random').then(res => {
+    return res.json();
+}).then(data => {
+    console.log(data.value);
+});
